cf2d8640ba47eecea963ccdf006e86515454332a
Riya <bhandari28riya@gmail.com>
2025-02-25 21:18:04 +0530

    feat(article-detail): added the dummy content

--- src/pages/article-detail/ArticleDetail.tsx
+++ src/pages/article-detail/ArticleDetail.tsx
@@ -1,8 +1,18 @@
-import { useParams } from 'react-router-dom';
+import articleData from '@assets/json/articleDetail.json';
+import { Typography } from '@mui/material';
+import parse from 'html-react-parser';
 
 const ArticleDetail = () => {
-  const { id } = useParams<string>();
-  return <div>{id}</div>;
+  return (
+    <div>
+      {articleData.map((article) => (
+        <div key={article.id}>
+          <Typography>{article.title}</Typography>
+          {parse(article.main_content)}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ArticleDetail;
