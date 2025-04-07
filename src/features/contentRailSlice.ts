e9009680d4dac733a7525651933568f5ee9fa8e5
Riya <bhandari28riya@gmail.com>
2025-04-07 17:40:34 +0530

    feat(contentRail): added the content rail

---
+++ src/features/contentRailSlice.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+export const contentRailSlice = createApi({
+  reducerPath: 'contentRail',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+  }),
+  endpoints: (builder) => ({
+    getContentRail: builder.query({
+      query: () => '/contentRail',
+    }),
+  }),
+});
+
+export const { useGetContentRailQuery } = contentRailSlice;
