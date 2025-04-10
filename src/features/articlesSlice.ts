eab8152ed32044acee7c0db420b6887b99eb598b
Riya <bhandari28riya@gmail.com>
2025-04-10 20:36:41 +0530

    feat(types): added types for tag and responses

--- src/features/articlesSlice.ts
+++ src/features/articlesSlice.ts
@@ -1,3 +1,4 @@
+import { IArticleDetail, IGetArticlesResponse } from '@/types/article';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // Create API Slice
@@ -7,17 +8,26 @@ export const articlesSlice = createApi({
     baseUrl: import.meta.env.VITE_API_BASE_URL,
   }),
   endpoints: (builder) => ({
-    getArticlesByTag: builder.query({
+    getArticlesByTag: builder.query<
+      IGetArticlesResponse,
+      { tagId: string; page: number }
+    >({
       query: ({ tagId, page }: { tagId: string; page: number }) =>
         `/articles/tag/${tagId}?page=${page}`,
     }),
-    getArticleById: builder.query({
+    getArticleById: builder.query<IArticleDetail, string>({
       query: (articleId) => `/articles/${articleId}`,
     }),
-    getAllArticles: builder.query({
+    getAllArticles: builder.query<IGetArticlesResponse, number>({
       query: (page) => `/articles?page=${page}`,
     }),
-    getAllArticlesByCategoryId: builder.query({
+    getAllArticlesByCategoryId: builder.query<
+      IGetArticlesResponse,
+      {
+        categoryId: string;
+        page: number;
+      }
+    >({
       query: ({ categoryId, page }) =>
         `/articles/category/${categoryId}?page=${page}`,
     }),
--- src/pages/articles/Articles.tsx
+++ src/pages/articles/Articles.tsx
@@ -16,32 +16,34 @@ const Articles = () => {
   console.log(articlesJson);
 
   return (
-    <Stack justifyContent={'center'} alignItems={'center'} gap={3}>
-      <Heading title={'Articles'} />
-      <Stack
-        direction={'row'}
-        justifyContent={{
-          xs: 'center',
-          sm: 'center',
-          md: 'centre',
-          lg: 'flex-start',
-          xl: 'flex-start',
-        }}
-        alignItems={'center'}
-        flexWrap={'wrap'}
-        gap={2}
-        marginLeft={{
-          md: '1rem',
-        }}
-      >
-        {articlesJson.articles.map((article: IArticle) => (
-          <Card key={article._id} article={article} />
-        ))}
+    articlesJson && (
+      <Stack justifyContent={'center'} alignItems={'center'} gap={3}>
+        <Heading title={articlesJson.name} />
+        <Stack
+          direction={'row'}
+          justifyContent={{
+            xs: 'center',
+            sm: 'center',
+            md: 'centre',
+            lg: 'flex-start',
+            xl: 'flex-start',
+          }}
+          alignItems={'center'}
+          flexWrap={'wrap'}
+          gap={2}
+          marginLeft={{
+            md: '1rem',
+          }}
+        >
+          {articlesJson.articles.map((article: IArticle) => (
+            <Card key={article._id} article={article} />
+          ))}
+        </Stack>
+        {articlesJson.totalPages > 1 ? (
+          <Pagination totalPages={articlesJson.totalPages} setPage={setPage} />
+        ) : null}
       </Stack>
-      {articlesJson.totalPages > 1 ? (
-        <Pagination totalPages={articlesJson.totalPages} setPage={setPage} />
-      ) : null}
-    </Stack>
+    )
   );
 };
 
