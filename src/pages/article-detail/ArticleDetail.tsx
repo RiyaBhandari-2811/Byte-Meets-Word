564ef6895fbf0dc3d7fb3776d77168617427fc7c
Riya <bhandari28riya@gmail.com>
2025-06-24 19:49:32 +0530

    feat(article-detail): add admin edit button and refactor layout for article metadata

--- src/pages/article-detail/ArticleDetail.tsx
+++ src/pages/article-detail/ArticleDetail.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import Heading from '@/components/heading/Heading';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import parse, { DOMNode, Element, Text } from 'html-react-parser';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { androidstudio } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -9,6 +11,7 @@ import DoneAllIcon from '@mui/icons-material/DoneAll';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetArticleByIdQuery } from '@/features/articlesSlice';
+import { useSelector } from 'react-redux';
 
 export interface IArticle {
   title: string;
@@ -25,6 +28,10 @@ export interface IArticle {
 }
 
 const ArticleDetail: React.FC = () => {
+  const userStore = useSelector((state: any) => state.userStore);
+
+  console.log('userStore: ', userStore);
+
   const [copiedCode, setCopiedCode] = useState<string | null>(null);
   const { articleId } = useParams();
   const { data: article, isLoading } = useGetArticleByIdQuery(
@@ -141,17 +148,29 @@ const ArticleDetail: React.FC = () => {
         >
           {article?.subtitle}
         </Typography>
-        <Typography
-          sx={{
-            color: 'rgba(156, 163, 175, 1)',
-            fontSize: {
-              xs: '0.7rem',
-              sm: '0.9rem',
-              xl: '1.3rem',
-            },
-            letterSpacing: '0.5px',
-          }}
-        >{`${article.createdAt} - ${article.readTime}`}</Typography>
+        <Stack
+          flexDirection={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+        >
+          <Typography
+            sx={{
+              color: 'rgba(156, 163, 175, 1)',
+              fontSize: {
+                xs: '0.7rem',
+                sm: '0.9rem',
+                xl: '1.3rem',
+              },
+              letterSpacing: '0.5px',
+            }}
+          >{`${article.createdAt} - ${article.readTime}`}</Typography>
+
+          {userStore.role === 'admin' && (
+            <Button color="info" sx={{ maxWidth: 'max-content' }}>
+              Edit
+            </Button>
+          )}
+        </Stack>
         <img src={article.featureImage} alt={article.title} />
         <Typography component={'div'}>
           {parse(article.mainContent, { replace: transform })}
